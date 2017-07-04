12d34f85169b6ac1e8415269338dfa72d4e35b50
AndreicaPaul <paulpaulandreica@gmail.com>
2017-07-04 18:07:27 +0300

    Integrated Mqtt connections.
    The android app is now subscribed to "nodemcu/#" topic from our mqtt
    server and can display the temperature and humidity when someone
    publishes to the topics: "nodemcu/temperature" and "nodemcu/humidity".

--- Weather-Station-Android-App/WeatherStation/app/src/main/java/com/example/paul/weatherstation/MainActivity.java
+++ Weather-Station-Android-App/WeatherStation/app/src/main/java/com/example/paul/weatherstation/MainActivity.java
@@ -12,8 +12,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ramotion.foldingcell.FoldingCell;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -22,6 +35,85 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //MQTT CONNECTION
+        String clientId = MqttClient.generateClientId();
+        final MqttAndroidClient client =
+                new MqttAndroidClient(getApplicationContext(), "tcp://m20.cloudmqtt.com:16691",clientId);
+        MqttConnectOptions options = new MqttConnectOptions();
+        options.setUserName("android");
+        options.setPassword("android".toCharArray());
+
+        try {
+            IMqttToken token = client.connect(options);
+            token.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    // We are connected
+                    Toast.makeText(getApplicationContext(), "Connected successfully", Toast.LENGTH_SHORT).show();
+                    int qos = 1;
+                    try {
+                        IMqttToken subToken = client.subscribe("nodemcu/#", qos);
+                        subToken.setActionCallback(new IMqttActionListener() {
+                            @Override
+                            public void onSuccess(IMqttToken asyncActionToken) {
+                                Toast.makeText(getApplicationContext(), "Subscribed successfully", Toast.LENGTH_SHORT).show();
+                            }
+
+                            @Override
+                            public void onFailure(IMqttToken asyncActionToken,
+                                                  Throwable exception) {
+                                // The subscription could not be performed, maybe the user was not
+                                // authorized to subscribe on the specified topic e.g. using wildcards
+                                Toast.makeText(getApplicationContext(), "Subscribed failed", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    } catch (MqttException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    // Something went wrong e.g. connection timeout or firewall problems
+                    Toast.makeText(getApplicationContext(), "Connection failed, please try again.", Toast.LENGTH_SHORT).show();
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+
+
+
+        client.setCallback(new MqttCallback() {
+            @Override
+            public void connectionLost(Throwable cause) {
+
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage message) throws Exception {
+                if(topic.equals("nodemcu/temperature")){
+                    TextView temperatureText = (TextView) findViewById(R.id.temperature_value_text);
+                    byte[] bytes = message.getPayload();
+                    String s = new String(bytes);
+                    temperatureText.setText(s);
+                }
+                else if(topic.equals("nodemcu/humidity")){
+                    TextView humidityText = (TextView) findViewById(R.id.humidity_level_text);
+                    byte[] bytes = message.getPayload();
+                    String s = new String(bytes);
+                    humidityText.setText(s);
+                }
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken token) {
+
+            }
+        });
+        //END OF MQTT
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
