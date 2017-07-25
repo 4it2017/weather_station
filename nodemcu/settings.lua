92c66ecaee46f6e82388ec69454e44d8ef7a5cfb
adam gligor <gligor.adam@gmail.com>
2017-07-25 12:27:50 +0200

    Create settings.lua

---
+++ nodemcu/settings.lua
@@ -0,0 +1,12 @@
+WIFI_SSID = "netmatch-guest"
+WIFI_PASS = "nmt2013gu3st"
+
+MQTT_SERVER = "m20.cloudmqtt.com"
+MQTT_PORT = 16691
+MQTT_USER = "idwlxdum"
+MQTT_PASS = "ksflFfLEc1M6"
+
+LOC_ALTITUDE = 300
+
+OPT_SAMPLING = 900000000 --15min
+  
