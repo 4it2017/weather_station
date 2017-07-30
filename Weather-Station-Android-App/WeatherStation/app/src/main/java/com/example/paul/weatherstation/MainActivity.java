e73e962883dd30191445f2f98435663824afe9bf
AndreicaPaul <paulpaulandreica@gmail.com>
2017-07-30 19:03:38 +0300

    Refactored the way we are connecting to the server.
    Removed the background service, for now, beacause it's redundant.

--- Weather-Station-Android-App/WeatherStation/app/src/main/java/com/example/paul/weatherstation/MainActivity.java
+++ Weather-Station-Android-App/WeatherStation/app/src/main/java/com/example/paul/weatherstation/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.paul.weatherstation;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,18 +14,32 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private SwipeRefreshLayout swipeRefreshLayout;
-    private MqttConnectionManagerService mqttConnectionManagerService = new MqttConnectionManagerService();
+    MqttAndroidClient mqttAndroidClient;
+    MqttConnectOptions mqttConnectOptions;
+    private final String serverUri = "tcp://m20.cloudmqtt.com:16691";
+    private final String refreshTopic = "nodemcu/requests";
+    private final String temperatureTopic = "nodemcu/temperature";
+    private final String humidityTopic = "nodemcu/humidity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mqttConnectionManagerService.initialize(this);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -46,11 +61,55 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
-                MainActivity.this.useMqtt(true);
+                publishRefreshMessage();
                 swipeRefreshLayout.setRefreshing(false);
             }
         });
 
+        //mqtt
+
+        this.mqttAndroidClient = this.createMqttAndroidClient();
+        this.mqttAndroidClient.setCallback(new MqttCallback() {
+            @Override
+            public void connectionLost(Throwable cause) {
+                Toast.makeText(MainActivity.this, "Connection lost!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage message) throws Exception {
+                if(topic.equals(temperatureTopic)){
+                    Toast.makeText(getApplicationContext(), "Recieved new temperature", Toast.LENGTH_SHORT).show();
+                    refreshTemperatureView(message.toString());
+                }
+                else if(topic.equals(humidityTopic)){
+                    Toast.makeText(getApplicationContext(), "Recieved new humidity", Toast.LENGTH_SHORT).show();
+                    refreshHumidityView(message.toString());
+                }
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken token) {
+
+            }
+        });
+
+        this.mqttConnectOptions = createMqttConnectOptions();
+
+        try {
+            mqttAndroidClient.connect(mqttConnectOptions, null, new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    subscribeToTopics();
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    Toast.makeText(MainActivity.this, "Something went wrong connecting to server.", Toast.LENGTH_SHORT).show();
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
     }
 
 
@@ -113,18 +172,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     }
 
-    @Override
-    protected void onResume(){
-        this.useMqtt(false);
-        super.onResume();
-    }
-
-    private void useMqtt(boolean needRefresh){
-        Intent intent = new Intent(this, this.mqttConnectionManagerService.getClass());
-        intent.putExtra("NEED_REFRESH",needRefresh);
-        startService(intent);
-    }
-
     public void refreshTemperatureView(String s){
         TextView temperatureText = (TextView) findViewById(R.id.temperature_value_text);
         temperatureText.setText(s);
@@ -134,4 +181,55 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         TextView humidityText = (TextView) findViewById(R.id.humidity_level_text);
         humidityText.setText(s);
     }
+
+    private MqttConnectOptions createMqttConnectOptions() {
+        MqttConnectOptions options = new MqttConnectOptions();
+        options.setUserName("android");
+        options.setPassword("android".toCharArray());
+        return options;
+    }
+
+    private MqttAndroidClient createMqttAndroidClient() {
+        String clientId = MqttClient.generateClientId();
+        return new MqttAndroidClient(getApplicationContext(), this.serverUri,clientId);
+    }
+
+    public void subscribeToTopics(){
+        try {
+            mqttAndroidClient.subscribe("nodemcu/#", 0, null, new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    Toast.makeText(MainActivity.this, "Something went wrong.", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+        } catch (MqttException ex){
+            System.err.println("Exception whilst subscribing");
+            ex.printStackTrace();
+        }
+    }
+
+    public void publishRefreshMessage(){
+
+        try {
+            mqttAndroidClient.publish(refreshTopic, getRefreshMessage());
+
+        } catch (MqttException e) {
+            System.err.println("Error Publishing: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public MqttMessage getRefreshMessage(){
+        String payload = "NEED REFRESH!";
+        byte[] encodedPayload;
+        encodedPayload = payload.getBytes();
+        return new MqttMessage(encodedPayload);
+    }
+
 }
--- Weather-Station-Android-App/WeatherStation/app/src/main/java/com/example/paul/weatherstation/MqttConnectionManagerService.java
+++ Weather-Station-Android-App/WeatherStation/app/src/main/java/com/example/paul/weatherstation/MqttConnectionManagerService.java
@@ -1,7 +1,10 @@
 package com.example.paul.weatherstation;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.widget.Toast;
@@ -17,6 +20,7 @@ import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
 
+
 /**
  * Created by Paul on 05-Jul-17.
  */
@@ -25,14 +29,7 @@ public class MqttConnectionManagerService extends Service{
 
     private MqttAndroidClient client;
     private MqttConnectOptions options;
-    private String refreshTopic = "nodemcu/requests";
-    private String temperatureTopic = "nodemcu/temperature";
-    private String humidityTopic = "nodemcu/humidity";
-    private static MainActivity activity;
 
-    public void initialize(MainActivity activity){
-        MqttConnectionManagerService.activity = activity;
-    }
 
     @Nullable
     @Override
@@ -47,11 +44,10 @@ public class MqttConnectionManagerService extends Service{
         client = createMqttAndroidClient();
     }
 
-
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        boolean needRefresh =(boolean) intent.getExtras().get("NEED_REFRESH");
-        this.connect(client, options, needRefresh);
+        if(isNetworkAvailable() && !client.isConnected())
+            this.connect(client, options);
         return START_STICKY;
     }
 
@@ -67,17 +63,14 @@ public class MqttConnectionManagerService extends Service{
         return new MqttAndroidClient(getApplicationContext(), "tcp://m20.cloudmqtt.com:16691",clientId);
     }
 
-    private void connect(final MqttAndroidClient client, MqttConnectOptions options, final boolean needRefresh)
-    {
+    private void connect(final MqttAndroidClient client, MqttConnectOptions options) {
         try {
-            if (!client.isConnected())
-            {
+            if (!client.isConnected()) {
                 IMqttToken token = client.connect(options);
                 //on successful connection, publish or subscribe as usual
                 token.setActionCallback(new IMqttActionListener() {
                     @Override
                     public void onSuccess(IMqttToken asyncActionToken) {
-                        Toast.makeText(getApplicationContext(), "Connected successfully", Toast.LENGTH_SHORT).show();
                             //Subscribes
                             int qos = 1;
                             try {
@@ -85,14 +78,14 @@ public class MqttConnectionManagerService extends Service{
                                 subToken.setActionCallback(new IMqttActionListener() {
                                     @Override
                                     public void onSuccess(IMqttToken asyncActionToken) {
-                                        Toast.makeText(getApplicationContext(), "Subscribed successfully", Toast.LENGTH_SHORT).show();
+
                                     }
 
                                     @Override
                                     public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
                                         // The subscription could not be performed, maybe the user was not
                                         // authorized to subscribe on the specified topic e.g. using wildcards
-                                        Toast.makeText(getApplicationContext(), "Subscribed failed", Toast.LENGTH_SHORT).show();
+
                                     }
                                 });
                             } catch (MqttException e) {
@@ -102,7 +95,7 @@ public class MqttConnectionManagerService extends Service{
                     @Override
                     public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
                         // Something went wrong e.g. connection timeout or firewall problems
-                        Toast.makeText(getApplicationContext(), "Connection failed, please try again.", Toast.LENGTH_SHORT).show();
+
                     }
                 });
                 client.setCallback(new MqttCallback() {
@@ -114,14 +107,7 @@ public class MqttConnectionManagerService extends Service{
                     @Override
                     public void messageArrived(String topic, MqttMessage message) throws Exception {
 
-                        if(topic.equals(MqttConnectionManagerService.this.temperatureTopic)){
-                            Toast.makeText(MqttConnectionManagerService.this, "Recieved new temperature", Toast.LENGTH_SHORT).show();
-                            activity.refreshTemperatureView(getMessageText(message));
-                        }
-                        else if(topic.equals(MqttConnectionManagerService.this.humidityTopic)){
-                            Toast.makeText(MqttConnectionManagerService.this, "Recieved new humidity", Toast.LENGTH_SHORT).show();
-                            activity.refreshHumidityView(getMessageText(message));
-                        }
+
                     }
 
                     @Override
@@ -133,24 +119,13 @@ public class MqttConnectionManagerService extends Service{
         } catch (MqttException e) {
             e.printStackTrace();
         }
-        if(needRefresh) {
-            try {
-                client.publish(this.refreshTopic, this.getRefreshMessage());
-            } catch (MqttException e) {
-                e.printStackTrace();
-            }
-            Toast.makeText(this, "Publish successful", Toast.LENGTH_SHORT).show();
-        }
     }
 
-    private String getMessageText(MqttMessage mqttMessage){
-        return new String(mqttMessage.getPayload());
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
     }
 
-    private MqttMessage getRefreshMessage(){
-        String payload = "NEED REFRESH!";
-        byte[] encodedPayload;
-        encodedPayload = payload.getBytes();
-        return new MqttMessage(encodedPayload);
-    }
 }
